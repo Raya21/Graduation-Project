77d073e4228dfbcf2f32d0426ac45903908304a9
unknown <aya.q2000@gmail.com>
2022-10-06 19:49:31 +0300

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:index/forget_password.dart';
+import 'package:index/home.dart';
 import 'package:index/login.dart';
 
 void main() {
@@ -14,8 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
      debugShowCheckedModeBanner: false,
-     home: ForgetPass(),
-     
+     theme: ThemeData(
+      primaryColor: Colors.purple,
+     ),
+     home: Home(),
+     routes: {
+      "Home":(context) => Home(),
+      "ForgetPass":(context) => ForgetPass(),
+      "Login":(context) => Login()
+      },
     );
   }
 }
