5ebd431743f935f26fafd063975c88d0efef540f
unknown <aya.q2000@gmail.com>
2022-11-26 16:52:55 +0200

    adding search bar to outstanding students page part 1

--- lib/outstanding_students.dart
+++ lib/outstanding_students.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
+
 class outstandingStudents extends StatefulWidget {
   const outstandingStudents({super.key});
 
@@ -11,8 +12,9 @@ class outstandingStudents extends StatefulWidget {
 
 class _outstandingStudentsState extends State<outstandingStudents> {
   List list = [];
+
   Future GetData() async {
-    var url = "http://192.168.1.100/handinhand/outStudent.php";
+    var url = "http://192.168.1.113/handinhand/outStudent.php";
     var res = await http.get(Uri.parse(url));
     if (res.statusCode == 200) {
       var red = json.decode(res.body);
@@ -23,11 +25,28 @@ class _outstandingStudentsState extends State<outstandingStudents> {
     }
   }
 
+  List Data = [];
+  Future Suggestion() async {
+    var url = "http://192.168.1.113/handinhand/suggestion.php";
+    var res = await http.get(Uri.parse(url));
+    if (res.statusCode == 200) {
+      var data = jsonDecode(res.body);
+      for (int i = 0; i < data.length; i++) {
+        setState(() {
+          Data.add(data[i]['fname']);
+        });
+      }
+    }
+    print(Data);
+  }
+
   @override
   void initState() {
     super.initState();
+    Suggestion();
     GetData();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,6 +57,13 @@ class _outstandingStudentsState extends State<outstandingStudents> {
                 color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
           ),
           backgroundColor: Colors.purple,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showSearch(context: context, delegate: DataSearch(data: Data));
+                },
+                icon: Icon(Icons.search))
+          ],
           leading: IconButton(
             icon: Icon(
               Icons.arrow_back,
@@ -68,11 +94,51 @@ class _outstandingStudentsState extends State<outstandingStudents> {
                       style: TextStyle(fontSize: 20),
                     ),
                     leading: Container(
-                        child: Image.network("http://192.168.1.100/handinhand/"+list[i]["image"]),
+                      child: Image.network("http://192.168.1.113/handinhand/" +
+                          list[i]["image"]),
                     ),
                   ),
                 ),
               );
             }));
+  }
+}
+
+class DataSearch extends SearchDelegate {
+  List data = [];
+  DataSearch({required this.data});
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [IconButton(onPressed: () {
+      query = "";
+    }, icon: Icon(Icons.close))];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(onPressed: () {
+      close(context, null);
+    }, icon: Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text(query,
+    style: TextStyle(fontSize: 20),);
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    var datasuggestion = query.isEmpty ? data : data.where((element) => element.toString().toLowerCase().contains(query)).toList();
+    return ListView.builder(
+      itemCount: datasuggestion.length,
+    itemBuilder: (ctn,index){
+      return ListTile(
+        onTap: (){
+          query = datasuggestion[index];
+          showResults(context);
+        },
+        leading: Icon(Icons.person),
+        title: Text('${datasuggestion[index]}'),);});
   }
 }
