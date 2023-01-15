37231ed2ceb8b1cb0a6cb65acc38c7b816ea45fc
unknown <aya.q2000@gmail.com>
2023-01-15 19:20:24 +0200

    some edits

--- phpfiles/personInfo.php
+++ phpfiles/personInfo.php
@@ -16,18 +16,18 @@ $gender = $_POST['gender'];
 $socialstatus = $_POST['socialstatus'];
 $flag1 = true;
 
-$sql = "SELECT * FROM person_info WHERE id='".$id."'";
+$sql = "SELECT * FROM person_info WHERE email='".$email."'";
 $result = mysqli_query($mydb,$sql);
 $count  = mysqli_num_rows($result);
 
 
 
 if ($count == 1) {
-    $update = "UPDATE person_info SET  email='".$email."', fname='".$fname."', birthdate='".$birthdate."', birthplace='".$birthplace."', city='".$city."', income='".$income."', gender='".$gender."', socialstatus='".$socialstatus."', flag1='".$flag1."' WHERE id='".$id."'";
+    $update = "UPDATE person_info SET  id='".$id."', fname='".$fname."', birthdate='".$birthdate."', birthplace='".$birthplace."', city='".$city."', income='".$income."', gender='".$gender."', socialstatus='".$socialstatus."', flag1='".$flag1."' WHERE email='".$email."'";
     $query = mysqli_query($mydb,$update);
     echo json_encode("Updated");
 }else{
-    $insert = "INSERT INTO person_info(id,email,fname,birthdate,birthplace,city,income,gender,socialstatus)VALUES('".$id."','".$email."','".$fname."','".$birthdate."','".$birthplace."','".$city."','".$income."','".$gender."','".$socialstatus."', flag1='".$flag1."')";
+    $insert = "INSERT INTO person_info(id,email,fname,birthdate,birthplace,city,income,gender,socialstatus,flag1)VALUES('".$id."','".$email."','".$fname."','".$birthdate."','".$birthplace."','".$city."','".$income."','".$gender."','".$socialstatus."','".$flag1."')";
     $query = mysqli_query($mydb,$insert);
     if ($query) {
         echo json_encode("Success");
